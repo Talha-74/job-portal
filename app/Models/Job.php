326a23b19758e4abf9e8c485ff990a7463f837d1
Talha7447 <talhashinwari7474@gmail.com>
2023-12-30 23:49:00 +0500

    post job model and migration set up

---
+++ app/Models/Job.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'job_title',
+        'job_region',
+        'job_type',
+        'vacancy',
+        'experience',
+        'salary',
+        'gender',
+        'application_deadline',
+        'job_description',
+        'responsibilities',
+        'education_and_experience',
+        'other_benefits'
+    ];
+}
